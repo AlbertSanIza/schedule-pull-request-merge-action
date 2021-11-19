d8296dd0ba94d19dffe6f56c1e3012ee9ce9b34a
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 15:40:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const core = require('@actions/core')
+
 const pullRequest = require('./lib/pullRequest.js')
 
 async function run() {
+    core.info(`Started at: ${new Date().toISOString()}`)
     pullRequest()
+    core.info(`Ended at: ${new Date().toISOString()}`)
 }
 
 run()
