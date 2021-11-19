f3b28107be3a16deaa620e3a4f99be92a54d1fe3
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 14:39:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ async function run() {
 }
 
 run()
-
-process.on('unhandledRejection', (reason, promise) => {
-    core.warning('Unhandled Rejection at:')
-    core.warning(promise)
-    core.setFailed(reason)
-})
