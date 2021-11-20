893ca13b7f2e1e29a0abb16fecd83d6bd4a5207c
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 17:07:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pullRequest = require('./lib/pullRequest.js')
 const localeDate = require('./lib/locale.js')
 
 async function run() {
+    core.info(`Timezone: ${core.getInput('time_zone')}`)
     core.info(`Started at: ${localeDate(new Date())}`)
     pullRequest()
     core.info(`Ended at: ${localeDate(new Date())}`)
