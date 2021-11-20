6fb29af56e245bd9ce4fff6fc89a3e5938f21d1a
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 17:02:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core')
 
 const pullRequest = require('./lib/pullRequest.js')
+const localeDate = require('./lib/locale.js')
 
 async function run() {
-    core.info(`Started at: ${new Date().toISOString()}`)
+    core.info(`Started at: ${localeDate(new Date())}`)
     pullRequest()
-    core.info(`Ended at: ${new Date().toISOString()}`)
+    core.info(`Ended at: ${localeDate(new Date())}`)
 }
 
 run()
