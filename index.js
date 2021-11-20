b09850efbefbae7dfe6d64a39c9ff59bb0afd4c5
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 17:55:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const localeDate = require('./lib/locale.js')
 async function run() {
     core.info(`Timezone: ${core.getInput('time_zone')}`)
     core.info(`Started at: ${localeDate(new Date())}`)
-    pullRequest()
+    if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
+        pullRequest()
+    } else {
+        core.info(`Should attempt merge`)
+    }
     core.info(`Ended at: ${localeDate(new Date())}`)
 }
 
