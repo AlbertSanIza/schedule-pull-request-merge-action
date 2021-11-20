d24c7dd6743a2e960bd759ad6d268b23e9c3126b
Albert Sanchez <albert.san.iza@gmail.com>
2021-11-19 18:00:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const core = require('@actions/core')
 
 const pullRequest = require('./lib/pullRequest.js')
 const localeDate = require('./lib/locale.js')
+const schedule = require('./lib/schedule.js')
 
 async function run() {
     core.info(`Timezone: ${core.getInput('time_zone')}`)
     core.info(`Started at: ${localeDate(new Date())}`)
     if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
+        core.info('Handle Pull Request Action')
         pullRequest()
     } else {
-        core.info(`Should attempt merge`)
+        core.info('Schedule Action')
+        schedule()
     }
     core.info(`Ended at: ${localeDate(new Date())}`)
 }
